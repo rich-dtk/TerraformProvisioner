d7894da1d56ccb942e8a321f23b5c764769d7155
Rich Pelavin <rich@react08.com>
2019-09-27 19:43:47 +0000

    update

--- variables.tf
+++ variables.tf
@@ -1,38 +1,35 @@
 # Variables
-variable "access_key" {
-	default = "ACCESS_KEY_HERE"
-}
-variable "secret_key" {
-	default = "SECRET_KEY_HERE"
-}
+
 variable "region" {
-  default = "us-east-2"
+  default = "us-east-1"
 }
-variable "cidr_vpc" {
-  description = "CIDR block for the VPC"
-  default = "10.1.0.0/16"
-}
-variable "cidr_subnet" {
-  description = "CIDR block for the subnet"
-  default = "10.1.0.0/24"
-}
-variable "availability_zone" {
-  description = "availability zone to create subnet"
-  default = "us-east-2a"
-}
+
 variable "public_key_path" {
   description = "Public key path"
   default = "~/.ssh/id_rsa.pub"
 }
 variable "instance_ami" {
   description = "AMI for aws EC2 instance"
-  default = "ami-0cf31d971a3ca20d6"
+  default = "ami-07d0cf3af28718ef8"
 }
 variable "instance_type" {
   description = "type for aws EC2 instance"
   default = "t2.micro"
 }
+
+variable "subnet_id" {
+  default = "subnet-9a093dc6"
+}
+
+variable "security_group_id" {
+  default = "sg-0bc45a72e4ed9f573"
+}
+
+variable "key_pair_name" {
+   default = "richp"
+}
+
 variable "environment_tag" {
   description = "Environment tag"
-  default = "Production"
+  default = "Test"
 }
